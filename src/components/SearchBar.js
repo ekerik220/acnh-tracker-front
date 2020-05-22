b7fb658c9fc1b3595aaa49b10494af04927a1780
Evan Kerik <ekerik220@gmail.com>
2020-05-22 17:34:31 +0900

    Fixed issue where search bar got bigger on zooming out

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,46 +1,56 @@
 import React from "react";
-import { InputGroup, FormControl, Button, Form } from "react-bootstrap";
+import { InputGroup, Button } from "react-bootstrap";
 import styled from "styled-components";
 
 export default function SearchBar() {
   return (
     <Wrapper>
-      <InputGroup>
-        <InputGroup.Prepend>
-          <CategorySelect as="select" custom>
-            <option>Wall-mounted</option>
-            <option>...</option>
-          </CategorySelect>
-        </InputGroup.Prepend>
-        <SearchInput placeholder="Search" />
-        <InputGroup.Append>
-          <SearchButton variant="primary">
-            <i class="fas fa-search"></i>
-          </SearchButton>
-        </InputGroup.Append>
-      </InputGroup>
+      <InputGroup.Prepend>
+        <CategorySelect>
+          <option>Wall-mounted</option>
+          <option>...</option>
+        </CategorySelect>
+      </InputGroup.Prepend>
+      <SearchInput placeholder="Search" />
+      <InputGroup.Append>
+        <SearchButton variant="primary">
+          <i class="fas fa-search"></i>
+        </SearchButton>
+      </InputGroup.Append>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: 500px;
-  display: flex;
-`;
+const CategorySelect = styled.select`
+  border: 1px solid #edeff1;
+  border-radius: 3px;
+  padding: 5px;
+  background: white;
 
-const CategorySelect = styled(Form.Control)`
   @media (max-width: 768px) {
     font-size: 12px;
+    width: 70px;
   }
 `;
 
-const SearchInput = styled(FormControl)`
+const SearchInput = styled.input`
+  border: 1px solid #edeff1;
+  padding-left: 8px;
+
   @media (max-width: 768px) {
     font-size: 12px;
+    width: 100px;
   }
 `;
 
 const SearchButton = styled(Button)`
-  font-size: 12px;
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const Wrapper = styled(InputGroup)`
+  flex-wrap: nowrap;
+  max-width: 500px;
+  justify-content: flex-end;
 `;
