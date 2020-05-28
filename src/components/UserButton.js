08b1c124dfb655a5aca34944ee39e989a1b8171e
Evan Kerik <ekerik220@gmail.com>
2020-05-28 19:08:55 +0900

    Change button color when logged in. Change menu options when logged in. Add logout function.

--- src/components/UserButton.js
+++ src/components/UserButton.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { Dropdown } from "react-bootstrap";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { setUserName, setLoginToken } from "../redux/actions";
 
 export default function UserButton() {
+  // If these are both non-null, then we are logged in
+  const userName = useSelector((state) => state.userName);
+  const loginToken = useSelector((state) => state.loginToken);
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(setUserName(null));
+    dispatch(setLoginToken(null));
+  };
+
   return (
     <Dropdown>
-      <DropdownToggle variant="secondary" className="ml-3">
+      <DropdownToggle
+        variant={userName && loginToken ? "primary" : "secondary"}
+        className="ml-3"
+      >
         <i className="fas fa-user"></i>
       </DropdownToggle>
-      <Dropdown.Menu>
-        <Dropdown.Item href="/register">Register</Dropdown.Item>
-      </Dropdown.Menu>
+      {userName && loginToken ? (
+        <Dropdown.Menu>
+          <Dropdown.Item disabled>{userName}</Dropdown.Item>
+          <Dropdown.Item onSelect={logout}>Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      ) : (
+        <Dropdown.Menu>
+          <Dropdown.Item href="/login">Login</Dropdown.Item>
+          <Dropdown.Item href="/register">Register</Dropdown.Item>
+        </Dropdown.Menu>
+      )}
     </Dropdown>
   );
 }
