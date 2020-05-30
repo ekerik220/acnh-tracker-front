7338bbcfb5e71d709876e6d3be0907583a922672
Evan Kerik <ekerik220@gmail.com>
2020-05-30 20:26:04 +0900

    Added circular progress bar component

---
+++ src/components/CircularProgress.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { easeQuadInOut } from "d3-ease";
+import AnimatedProgressProvider from "./AnimatedProgressProvider";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+export default function CircularProgress({ percent, tooltip }) {
+  return (
+    <OverlayTrigger overlay={<Tooltip>{tooltip}</Tooltip>}>
+      <div style={{ userSelect: false, cursor: "default" }}>
+        <AnimatedProgressProvider
+          valueStart={0}
+          valueEnd={percent}
+          duration={1.4}
+          easingFunction={easeQuadInOut}
+        >
+          {(value) => {
+            const roundedValue = Math.round(value);
+            return (
+              <CircularProgressbar
+                value={value}
+                text={`${roundedValue}%`}
+                styles={buildStyles({ pathTransition: "none" })}
+              />
+            );
+          }}
+        </AnimatedProgressProvider>
+      </div>
+    </OverlayTrigger>
+  );
+}
