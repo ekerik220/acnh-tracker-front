a35d5cc38abdd71c2a9a8207ed44492e6f5e37ed
Evan Kerik <ekerik220@gmail.com>
2020-05-30 14:39:41 +0900

    Add code for the have button

--- src/components/HaveButton.js
+++ src/components/HaveButton.js
@@ -1,22 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import {
+  setItemData,
+  setErrorText,
+  setUserWishlist,
+  setUserList,
+} from "../redux/actions";
+import { useSelector, useDispatch } from "react-redux";
 
-export default function HaveButton() {
-  return <Button>Have</Button>;
+export default function WantButton(props) {
+  const loginToken = useSelector((state) => state.loginToken);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const addToWishList = async () => {
+    postToEndpoint("http://localhost:4000/list/addToList");
+  };
+
+  const removeFromWishList = async () => {
+    postToEndpoint("http://localhost:4000/list/listDelete");
+  };
+
+  const postToEndpoint = async (endpoint) => {
+    const body = {
+      item_name: props.itemName,
+      category: props.itemCategory,
+      variation:
+        props.itemVariation.name === "NA" ? null : props.itemVariation.name,
+    };
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json", "auth-token": loginToken },
+      body: JSON.stringify(body),
+    };
+
+    try {
+      setLoading(true);
+      const req = await fetch(endpoint, options);
+      const res = await req.json();
+      setLoading(false);
+
+      if (res.error) dispatch(setErrorText(res.error));
+      else {
+        dispatch(setUserWishlist(res.wishList));
+        dispatch(setUserList(res.list));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Button
+      className={loading ? "loading" : props.selected ? "selected" : null}
+      onClick={
+        loading ? null : props.selected ? removeFromWishList : addToWishList
+      }
+    >
+      {loading ? <i class="fas fa-spinner fa-spin"></i> : "Have"}
+    </Button>
+  );
 }
 
 const Button = styled.div`
   text-align: center;
   cursor: pointer;
   user-select: none;
-  background: grey;
-  border: none;
+  border: 1px solid #2ecc40;
   border-radius: 4px;
   margin: 1px;
   width: 100%;
 
-  &:hover {
-    background: rgba(0, 0, 0, 0.4);
-    transition: background-color 0.2s;
+  &.selected {
+    background: #2ecc40;
+  }
+
+  &.loading {
+    background: #2ecc40;
+    opacity: 0.5;
   }
 `;
