45ac1d8061c5d47c68d03f7ceb582ae1be022f80
Evan Kerik <ekerik220@gmail.com>
2020-05-31 16:10:32 +0900

    Beginnings of CatalogueItemArea component

---
+++ src/components/CatalogueItemArea.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import CatalogueItem from "./CatalogueItem";
+
+export default function CatalogueItemArea({ displayedList }) {
+  return (
+    <Wrapper>
+      {displayedList.map((item) => (
+        <CatalogueItem item={item} />
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  border: 1px solid black;
+  overflow-y: auto;
+`;
