9f68288649060c3708e782d075531ebe069f73f2
Evan Kerik <ekerik220@gmail.com>
2020-06-07 17:46:47 +0900

    Removed comment that isn't needed anymore

--- src/components/Catalogue/components/CategorySelect/useCategorySelect.js
+++ src/components/Catalogue/components/CategorySelect/useCategorySelect.js
@@ -5,9 +5,6 @@ import { setCategory } from "redux/slices";
 export default function useCategorySelect() {
   const dispatch = useDispatch();
 
-  // The category list in the DOM isn't in the same order as the category list
-  // array that stores all the information about each category so we need some
-  // logic to match them up.
   const handleCategoryChange = (event) => {
     const value = event.target.value;
     dispatch(setCategory(value));
