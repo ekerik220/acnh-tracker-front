fcd290f15dd0aa91879571183a5bd0704c9fa3e3
Evan K <ekerik220@gmail.com>
2020-06-10 21:21:15 +0900

    Put content in the HomeScreen

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,9 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h3>Home</h3>
-    </div>
+    <Wrapper>
+      <CenterImage>
+        <div>
+          <img
+            src={require("assets/leaf.jpg")}
+            alt="Animal Crossing leaf logo"
+          />
+        </div>
+      </CenterImage>
+      <FooterTextArea>
+        <a target="_blank" href="https://tinyurl.com/acnh-sheet">
+          Data source
+        </a>
+      </FooterTextArea>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+`;
+
+const CenterImage = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+
+  img {
+    width: 200px;
+  }
+`;
+
+const FooterTextArea = styled.div`
+  text-align: center;
+`;
