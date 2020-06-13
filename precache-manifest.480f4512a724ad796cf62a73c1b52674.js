62de25af0be244a857dd5ff9167f4b9e2f8b839b
Evan Kerik <ekerik220@gmail.com>
2020-06-14 07:00:42 +0900

    Updates

---
+++ precache-manifest.480f4512a724ad796cf62a73c1b52674.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67a4d22eeb4896a439f9e9d1657a35d",
+    "url": "/acnh-tracker-front/index.html"
+  },
+  {
+    "revision": "961b4b62cc42a7d877d0",
+    "url": "/acnh-tracker-front/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "ca56796e377bb7f15750",
+    "url": "/acnh-tracker-front/static/css/main.cc162ea7.chunk.css"
+  },
+  {
+    "revision": "961b4b62cc42a7d877d0",
+    "url": "/acnh-tracker-front/static/js/2.1388c587.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/acnh-tracker-front/static/js/2.1388c587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca56796e377bb7f15750",
+    "url": "/acnh-tracker-front/static/js/main.8866a005.chunk.js"
+  },
+  {
+    "revision": "2ed923343b0c26ddabfb",
+    "url": "/acnh-tracker-front/static/js/runtime-main.cd1aa780.js"
+  },
+  {
+    "revision": "2084215fe71167406903d9e7b839a946",
+    "url": "/acnh-tracker-front/static/media/logo.2084215f.png"
+  }
+]);
